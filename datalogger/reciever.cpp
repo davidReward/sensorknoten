9bdf359c414621baecc2020aecfd1bd84024265d
David <dp@nbpreisss-suse.suse>
2016-11-07 08:39:03 +0100

    reciever added

---
+++ datalogger/reciever.cpp
@@ -0,0 +1,10 @@
+#include <iostream>            
+#include <RF24/RF24.h>            //Allgemeine Libary für NRF24L01+
+#include <AES.h> 
+
+ 
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
