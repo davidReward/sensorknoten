25449e5e0670a7e555f1d6555c684aa9e29f8e04
David <dp@nbpreisss-suse.suse>
2016-11-15 16:34:15 +0100

    Anpassungen am Decrypter

--- decrypter/decrypter.py
+++ decrypter/decrypter.py
@@ -3,17 +3,22 @@
 import re
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()
+key = ABCDEFGHIJKLMNOP
 print "Der Schluessel lautet: %s" % key
 cipher_suite = Fernet(key)
-cipher_text = cipher_suite.encrypt(b"A really secret message. Not for prying eyes.")
-print "Der verschluesselte Text lautet: %s" % cipher_text
-plain_text = cipher_suite.decrypt(cipher_text)
-print "Der entschluesselte Text lautet: %s " % plain_text
 
 
 data = open("data.temp", "r")
-
 for line in data:
-    if re.match("(.*)(L|l)ove(.*)", line):
-        print line,
+    #if re.match("(.*)(L|l)ove(.*)", line):
+       print line
+       cipher_text= line.partition(" ")
+       print cipher_text
+       #plain_text = cipher_suite.decrypt(cipher_text)
+       #print "Der entschluesselte Text lautet: %s " % plain_text
+
+       
+
+ 
+
+
