0f4468f738929417e589ecdf3f67ef5439cab311
hansxy <preissdavid.de@gmail.com>
2016-11-22 10:19:34 +0000

    try another way to parse fifo

---
+++ dataProcessor_pipe.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import base64
+from struct import *
+from collections import namedtuple
+from shutil import copyfile
+
+
+
+data = open("datafifo", "r")
+while True:
+#for line in data:
+       ## hier line = readline
+       receivedData= line.partition(" ")
+       decodedData=base64.b64decode(receivedData[2])             
+       stationID, value, unit, timeid = unpack('<hfhL', decodedData)
+       print "ID: %d Value: %f , Unit: %d, TimeID: %d" % (stationID, value, unit, timeid)
+       
+data.close()
+
