77a63cf10bd2779a8f2616ade5212cd1c5ddecf5
hansxy <preissdavid.de@gmail.com>
2016-11-23 11:32:46 +0000

    problem with reading pipe fixed

--- dataProcessor_pipe.py
+++ dataProcessor_pipe.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 import base64
+import time
 from struct import *
-from collections import namedtuple
-from shutil import copyfile
-
-
 
 data = open("datafifo", "r")
-while True:
-#for line in data:
-       ## hier line = readline
-       receivedData= line.partition(" ")
-       decodedData=base64.b64decode(receivedData[2])             
-       stationID, value, unit, timeid = unpack('<hfhL', decodedData)
-       print "ID: %d Value: %f , Unit: %d, TimeID: %d" % (stationID, value, unit, timeid)
+while True:       
+       #eine Zeile aus Pipe lesen
+       dataLine= data.readline()
        
-data.close()
+       # 29 ist die Laenge der Daten
+       if 29 == len(dataLine):       
+            
+            #Daten von Zeitstempel trennen
+            receivedData= dataLine.partition(" ")
+            
+            # Daten sind in base64 kodiert, also dekodieren:
+            decodedData=base64.b64decode(receivedData[2])             
+            
+            # Muster um aus Bytes Struct zu rekonstruieren
+            stationID, value, unit, timeid = unpack('<hfhL', decodedData)
+            print "ID: %d Value: %f , Unit: %d, TimeID: %d" % (stationID, value, unit, timeid)
+       else:
+            # wenn keine Daten in der Pipe, dann 3 Sekuden warten
+            time.sleep(3)
 
