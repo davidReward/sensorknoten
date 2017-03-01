c699a2a367cae707e3b92cbd8050ecde766f5afd
hansxy <preissdavid.de@gmail.com>
2017-03-01 18:35:12 +0100

    using base64 for decoding

--- receiverMesh.py
+++ receiverMesh.py
@@ -6,9 +6,7 @@ from RF24 import *
 import base64
 import pdb
 import RPi.GPIO as GPIO
-import os, sys
-import struct
-import binascii
+from struct import *
 
 irq_gpio_pin = None
 
@@ -18,7 +16,7 @@ pipes = ["1Node", "2Node"]
 
 def setup():
     radio.begin()
-    radio.payloadSize = 6
+    radio.payloadSize = 28
     radio.enableDynamicPayloads()
     radio.setAutoAck(1)
     radio.setDataRate(RF24_250KBPS)  # 250kbs
@@ -33,23 +31,37 @@ def setup():
 
 
 def writeToFile(received_payload):
-    #print('Got payload size={} value="{}"'.format(radio.payloadSize, received_payload.decode('utf-8')))
+    # print('Got payload size={} value="{}"'.format(radio.payloadSize, received_payload.decode('utf-8')))
 
     # Timestamp erzeugen
-    utime = int(time.time())
+    utime_str = str(int(time.time()))
 
-    # Hier aktuelle Zeit einfuegen
-    data.write("1481278800" + " " + received_payload + " \n")  # % utime
+    data.write(utime_str + " " + received_payload + " \n")
     data.flush()
 
+def writeToDatabase(ID_hased, originAddr, value, unit):
+    print "Test"
+
+def genearteID_hashed(stationID, messageID, timeID):
+    str1=str(stationID)
+    str2=str(messageID)
+    str3=str(timeID)
+    toHash=str1+str2+str3
+    hashed=0 # genrate Hash here
+    return hashed
 
 
 def receive():
     if radio.available():
         while radio.available():
             receive_payload = radio.read(radio.payloadSize)
-            #print "%s" % receive_payload
-            writeToFile(receive_payload)
+            print "Kodiert: %s" % receive_payload
+            decodedData = base64.b64decode(receive_payload)
+            destinationAddr, originAddr, lastHopAddr, messageID, stationID, value, unit, timeid = unpack('<hhhhhfhL', decodedData)
+            print "destinationAddr: %d , originAddr: %d , lastHopAddr: %d , messageID: %d , ID: %d Value: %f , Unit: %d, TimeID: %d" % (destinationAddr,originAddr,lastHopAddr,messageID, stationID, value, unit, timeid)
+            print "Dekodiert: %s" % decodedData
+
+            #writeToFile(receive_payload)
 
 
 # Hauptprogramm:
