f843ca6f42c090d08eeae1c5eed85231e7fdc1bf
hansxy <preissdavid.de@gmail.com>
2017-03-07 17:09:27 +0100

    tool for reading db

--- getData.py
+++ getData.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 import sqlite3
 
-DBconn = sqlite3.connect('node1.db')
-queryCurs=DBconn.cursor()
 
-query = DBconn.execute('SELECT * FROM messwerte')
+def queryDB(table, col, value):
+    DBconn = sqlite3.connect('node1.db')
+    queryCurs = DBconn.cursor()
+    queryCurs.execute('SELECT * FROM {SQLtable} WHERE {SQLcol}={SQLvalue}'. \
+                      format(SQLtable=table, SQLcol = col, SQLvalue = value))
+    result = queryCurs.fetchall()
+    print result
+    DBconn.close()
 
-DBconn.close()
+table="messwerte"
+col = "value"
+value="31.0"
+
+print queryDB(table, col, value)
