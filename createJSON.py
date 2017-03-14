d825387c1869e08d113d3169c2d0e9bb1355817e
hansxy <preissdavid.de@gmail.com>
2017-03-14 16:59:48 +0100

    improve REST-API

--- createJSON.py
+++ createJSON.py
@@ -1,16 +1,18 @@
 import sqlite3
 import json
+import sys
 
-
-def createJSON():
-    print 'Hello'
-
-def queryDB(table, col, value):
+def queryDB(table, col, value, limit):
     DBconn = sqlite3.connect('node1.db')
+    # This enables column access by name: row['column_name']
+    DBconn.row_factory = sqlite3.Row
     queryCurs = DBconn.cursor()
-    queryCurs.execute('SELECT * FROM {SQLtable} WHERE {SQLcol}={SQLvalue}'. \
-                      format(SQLtable=table, SQLcol = col, SQLvalue = value))
 
-    json_string = json.dumps(queryCurs.fetchall())
+    queryCurs.execute('SELECT * FROM {SQLtable} WHERE {SQLcol}={SQLvalue} ORDER BY timestamp DESC LIMIT {SQLlimit}'. \
+                      format(SQLtable=table, SQLcol=col, SQLvalue=value, SQLlimit=limit))
+
+    row = queryCurs.fetchall()
+    row_json = [ dict(rec) for rec in row ]
+
     DBconn.close()
-    return json_string
+    return row_json
--- getData_web.py
+++ getData_web.py
@@ -2,27 +2,12 @@ import sqlite3
 from flask import Flask, jsonify, abort, make_response, url_for
 from flask.ext.httpauth import HTTPBasicAuth
 from sqlobject import *
-import simplejson
 from createJSON import *
 
 auth = HTTPBasicAuth()
 
 app = Flask(__name__)
 
-mdata = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
 
 
 def make_public_mdatum(mdatum):
@@ -49,25 +34,12 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.route('/todo/api/v1.0/mdata', methods=['GET'])
+@app.route('/mdata/station/<int:station>', methods=['GET'])
 @auth.login_required
-def get_mdata():
-    return jsonify({'mdata': [make_public_mdatum(mdatum) for mdatum in mdata]})
-
-@app.route('/todo/api/v1.0/mdata/<int:mdatum_id>', methods=['GET'])
-@auth.login_required
-def get_mdatum(mdatum_id):
-    mdatum = [mdatum for mdatum in mdata if mdatum['id'] == mdatum_id]
-    if len(mdatum) == 0:
-        abort(404)
-    return jsonify({'mdata': [make_public_mdatum(mdatum[0])]})
+def get_mdataall(station):
+    query_result = queryDB('messwerte','originAddr',station,1)
+    return jsonify({'Messdaten': [query_result]})
 
 
 if __name__ == '__main__':
-    table="messwerte"
-    col = "value"
-    value="31.0"
-
-    print queryDB(table, col, value)
-
     app.run(host='0.0.0.0', debug=True)
