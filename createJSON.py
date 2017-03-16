f50f9ca4fcfbe657a338091a78f2fb2cdc0f5a62
hansxy <preissdavid.de@gmail.com>
2017-03-16 12:48:02 +0100

    sql improvements

--- createJSON.py
+++ createJSON.py
@@ -8,14 +8,16 @@ def queryDB(table, col, value, limit):
     DBconn.row_factory = sqlite3.Row
     queryCurs = DBconn.cursor()
 
-    queryCurs.execute('SELECT * FROM {SQLtable} WHERE {SQLcol}={SQLvalue} ORDER BY timestamp DESC LIMIT {SQLlimit}'. \
+    # queryCurs.execute('SELECT * FROM {SQLtable} WHERE {SQLcol}={SQLvalue} ORDER BY timestamp DESC LIMIT {SQLlimit}'. \
+    #                   format(SQLtable=table, SQLcol=col, SQLvalue=value, SQLlimit=limit))
+
+
+    queryCurs.execute('SELECT MAX(timestamp), originAddr, unit, id, value FROM {SQLtable} WHERE {SQLcol}={SQLvalue} GROUP BY unit'. \
                       format(SQLtable=table, SQLcol=col, SQLvalue=value, SQLlimit=limit))
+
 
     row = queryCurs.fetchall()
     row_json = [ dict(rec) for rec in row ]
 
     DBconn.close()
     return row_json
-
-#use this:
-#SELECT MAX(timestamp),originAddr, unit, id, value from messwerte WHERE originAddr = 400 GROUP BY unit
