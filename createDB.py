b901db2b579850c7f93d8abceb023a59c11e713f
hansxy <preissdavid.de@gmail.com>
2017-03-30 17:31:16 +0200

    use hostname for db

--- createDB.py
+++ createDB.py
@@ -2,7 +2,7 @@ import MySQLdb as mdb
 from config import *
 
 #DBconn = sqlite3.connect(SQL_DB)
-DBconn = mdb.connect('localhost', 'root', 'hallotest123' 'node1')
+DBconn = mdb.connect('sensorknoteniot.it.dh-karlsruhe.de:3306', 'jan', 'hallotest123' 'node1')
 
 queryCurs=DBconn.cursor()
 
