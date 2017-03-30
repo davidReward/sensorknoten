fff0d3feeb576f80be312f26fbd8a7c4bd5d9973
hansxy <preissdavid.de@gmail.com>
2017-03-30 16:59:14 +0200

    fixes for mysql

--- createDB.py
+++ createDB.py
@@ -8,13 +8,13 @@ DBconn = pyodbc.connect(connString)
 queryCurs=DBconn.cursor()
 
 queryCurs.execute('''CREATE TABLE messwerte
-(id TEXT PRIMARY KEY ON CONFLICT IGNORE, originAddr INTEGER, value REAL, unit INTEGER, timestamp INTEGER)''')
+(id CHAR(32) PRIMARY KEY ON CONFLICT IGNORE, originAddr INTEGER, value DOUBLE, unit INTEGER, timestamp INTEGER)''')
 
 queryCurs.execute('''CREATE TABLE stationen
-(station_id INTEGER PRIMARY KEY, name TEXT, location TEXT, powerSaving INTEGER )''')
+(station_id INTEGER PRIMARY KEY, name VARCHAR(16), location VARCHAR(32), powerSaving BOOL )''')
 
 queryCurs.execute('''CREATE TABLE einheiten
-(unit_id INTEGER PRIMARY KEY, sensor TEXT, unit_name TEXT )''')
+(unit_id INTEGER PRIMARY KEY, sensor VARCHAR(16), unit_name VARCHAR(16) )''')
 
 
 DBconn.commit()
