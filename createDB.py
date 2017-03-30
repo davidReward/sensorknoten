947acf9fd99ec5de4e6f9df2d91affc71df623b3
hansxy <preissdavid.de@gmail.com>
2017-03-30 17:59:50 +0200

    change unitname to varchar32

--- createDB.py
+++ createDB.py
@@ -13,7 +13,7 @@ queryCurs.execute('''CREATE TABLE stationen
 (station_id INTEGER PRIMARY KEY, name VARCHAR(16), location VARCHAR(32), powerSaving BOOL )''')
 
 queryCurs.execute('''CREATE TABLE einheiten
-(unit_id INTEGER PRIMARY KEY, sensor VARCHAR(16), unit_name VARCHAR(16) )''')
+(unit_id INTEGER PRIMARY KEY, sensor VARCHAR(16), unit_name VARCHAR(32) )''')
 
 
 DBconn.commit()
