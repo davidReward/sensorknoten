8a59bbb3a772114cec4046d69c3bee60bd264e71
Jan Gerber <jan-456@t-online.de>
2017-04-05 14:32:30 +0200

    Anpassungen SQL Befehle und Cursor

--- createJSON.py
+++ createJSON.py
@@ -5,26 +5,25 @@ def queryDB_station(station):
     DBconn = mdb.connect(**config)
     # This enables column access by name: row['column_name']
     #DBconn.row_factory = sqlite3.Row
-    queryCurs = DBconn.cursor()
+    queryCurs = DBconn.cursor(dictionary=True)
 
     queryCurs.execute(
-         'SELECT MAX(timestamp) AS timestamp, originAddr, unit,unit_name,sensor ,ANY_VALUE(id), ANY_VALUE(value) '
+         'SELECT MAX(timestamp) AS timestamp, originAddr, unit,unit_name,sensor ,ANY_VALUE(id) AS id, ANY_VALUE(value) AS value '
          'FROM messwerte '
          'INNER JOIN einheiten ON messwerte.unit = einheiten.unit_id '
          'WHERE originAddr=%s GROUP BY unit',(station,))
 
     row = queryCurs.fetchall()
-    print row
     row_json = [ dict(rec) for rec in row ]
 
     DBconn.close()
     return row_json
 
 def queryDB_station_interval(station, unit, begin, end):
-    DBconn = mdb.connect(SQL_DB)
+    DBconn = mdb.connect(**config)
     # This enables column access by name: row['column_name']
     #DBconn.row_factory = sqlite3.Row
-    queryCurs = DBconn.cursor()
+    queryCurs = DBconn.cursor(dictionary=True)
 
     queryCurs.execute(
         'SELECT timestamp, originAddr, unit,unit_name,sensor ,ANY_VALUE(id) AS id, ANY_VALUE(value) AS value '
@@ -40,11 +39,11 @@ def queryDB_station_interval(station, unit, begin, end):
 
 
 def queryDB_id(id):
-    DBconn = mdb.connect(SQL_DB)
+    DBconn = mdb.connect(**config)
     # This enables column access by name: row['column_name']
     #DBconn.row_factory = sqlite3.Row
-    queryCurs = DBconn.cursor()
-    queryCurs.execute(
+    queryCurs = DBconn.cursor(dictionary=True)
+    anwser = queryCurs.execute(
         'SELECT * '
         'FROM messwerte '
         'WHERE id=%s', (id,))
@@ -54,10 +53,10 @@ def queryDB_id(id):
     return row_json
 
 def queryDBallStation():
-    DBconn = mdb.connect(SQL_DB)
+    DBconn = mdb.connect(**config)
     # This enables column access by name: row['column_name']
     #DBconn.row_factory = sqlite3.Row
-    queryCurs = DBconn.cursor()
+    queryCurs = DBconn.cursor(dictionary=True)
     queryCurs.execute(
         'SELECT originAddr, name , location, powerSaving '
         'FROM messwerte '
