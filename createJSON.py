d0bd0994abe84a05f3c13551ac0b25d47850bf6a
Jan Gerber <jan-456@t-online.de>
2017-04-19 12:27:21 +0200

    Anpassungen SQL Abfrage

--- createJSON.py
+++ createJSON.py
@@ -11,11 +11,13 @@ def queryDB_station(station):
     queryCurs = DBconn.cursor(dictionary=True)
 
     queryCurs.execute(
-	'SELECT a.timestamp , originAddr, a.unit ,id, value '
+	'SELECT a.timestamp , originAddr, a.unit ,id, value, unit_name,sensor '
          'FROM messwerte as a '
          'INNER JOIN '
          '(select max(timestamp) as timestamp, unit from messwerte where originAddr = %s group by unit ) as b '
-         'ON a.timestamp = b.timestamp and a.unit = b.unit ; ',(station,))
+         'ON a.timestamp = b.timestamp and a.unit = b.unit'
+		 'INNER JOIN einheiten ON messwerte.unit = einheiten.unit_id '
+         ' ; ',(station,))
          
 
     row = queryCurs.fetchall()
